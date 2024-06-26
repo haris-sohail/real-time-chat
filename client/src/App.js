f70f234eaf412103fb40152465559191d8ea1cba
Haris Sohail <harissohail.cs@gmail.com>
2024-06-26 10:58:02 +0500

    Add boiler plate code

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- server/database.js
+++ server/database.js
@@ -3,11 +3,19 @@ const { Pool } = require("pg");
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
+    database: "real_time_chat",
     password: "postgres",
     port: 5432
 });
 
-pool.query("CREATE DATABASE real_time_chat", (err, res) => {
+const createTblQry = `
+    CREATE TABLE IF NOT EXISTS accounts (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+    );`
+
+pool.query(createTblQry, (err, res) => {
     console.log(err, res);
 });
 
